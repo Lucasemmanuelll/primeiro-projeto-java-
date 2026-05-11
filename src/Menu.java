2473684d2b7b648825652668c7fe7c547c038438
Lucas Emmanuel <programacaolucas3@gmail.com>
2026-05-11 12:23:37 -0300

    refactor: Reorganização Menu.java test: testando método de login e cadastro

--- src/Menu.java
+++ src/Menu.java
@@ -1,22 +1,56 @@
+//1 - Criar conta: nome de acesso e senha.
+//2 - Depositar
+//3 - Sacar
+//4 - Mostrar dados
+//5 - Encerrar
+
+import java.util.Scanner;
 public class Menu {
-    private String nomeConta;
+    Scanner sc = new Scanner(System.in);
+    private int cpfUsuario;
     private double loginSenha;
 
-    public Menu(String nomeConta,double loginSenha){
+    public Menu(int cpfUsuario,double loginSenha){
         this.loginSenha = loginSenha;
-        this.nomeConta = nomeConta;
+        this.cpfUsuario = cpfUsuario ;
     }
-    public String getNomeConta(){
-        return nomeConta;
+    public int getCpfusuario(){
+        return cpfUsuario;
     }
     public double getLoginSenha(){
-        return getLoginSenha();
+        return loginSenha;
+    }
+                public void setCpfUsuario(int cpfUsuario){
+
+                        this.cpfUsuario= cpfUsuario;
+                }
+                public void setLoginSenha(double loginSenha){
+
+                        this.loginSenha = loginSenha;
+                }
+
+    public void Loginusuario(int cpf, int senha){
+        System.out.println("Digite seu CPF de acesso: ");
+        int cpf = sc.nextInt();
+        System.out.println("Digite sua SENHA de acesso: ");
+        int senha = sc.nextInt();
+        cpfUsuario = cpf;
+        loginSenha = senha;
+        if(cpfUsuario == ){//cadastro do usuário armazenado
+
+        }else{
+            System.out.println("CPF inexistente, tente se cadastrar."// + class cadastro  );
+        }
+    }
+        public void Cadastrousuario(){
+
+        }
     }
 
-
-
-
     public  void mostrarDados(){
-
+        System.out.println ("""
+                CPF de Acesso: 
+                Senha de Acesso: 
+                """);
     }
 }
