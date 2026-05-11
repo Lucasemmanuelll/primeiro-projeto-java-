e18d67db5c867e3ce556978434874ac1b6c3ca81
Lucas Emmanuel <programacaolucas3@gmail.com>
2026-05-11 03:10:12 -0300

    criando class Menu base

---
+++ src/Menu.java
@@ -0,0 +1,22 @@
+public class Menu {
+    private String nomeConta;
+    private double loginSenha;
+
+    public Menu(String nomeConta,double loginSenha){
+        this.loginSenha = loginSenha;
+        this.nomeConta = nomeConta;
+    }
+    public String getNomeConta(){
+        return nomeConta;
+    }
+    public double getLoginSenha(){
+        return getLoginSenha();
+    }
+
+
+
+
+    public  void mostrarDados(){
+
+    }
+}
