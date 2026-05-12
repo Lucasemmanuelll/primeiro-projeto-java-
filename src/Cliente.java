6b589a33f658cee4ad1ca028e5afdb868b636378
Lucas Emmanuel <programacaolucas3@gmail.com>
2026-05-12 20:07:35 -0300

    docs: criando class cliente

---
+++ src/Cliente.java
@@ -0,0 +1,71 @@
+public class Cliente {
+        private String nome;
+        private int idade;
+        private String cidade;
+        private String cpf;
+
+        public Cliente(
+                String nome,
+                int idade,
+                String cidade,
+                String cpf
+        ) {
+
+            this.nome = nome;
+            this.idade = idade;
+            this.cidade = cidade;
+            this.cpf = cpf;
+        }
+
+        public void exibirInformacoes() {
+
+            System.out.println(
+                    "Nome: " + getNome()
+            );
+
+            System.out.println(
+                    "Idade: " + getIdade() + " anos"
+            );
+
+            System.out.println(
+                    "Cidade: " + getCidade()
+            );
+
+            System.out.println(
+                    "CPF: " + getCpf()
+            );
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public void setCidade(String cidade) {
+            this.cidade = cidade;
+        }
+
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public String getCidade() {
+            return cidade;
+        }
+
+        public String getCpf() {
+            return cpf;
+        }
+    }
+
