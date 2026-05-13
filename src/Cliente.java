360f57eea215e5a613d0d4ea6d74a626eeb878be
Lucas Emmanuel <programacaolucas3@gmail.com>
2026-05-12 23:35:08 -0300

    docs:Acrescentando toString na classe cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,65 +1,57 @@
 public class Cliente {
-        private String nome;
-        private int idade;
-        private String cidade;
-        private String cpf;
 
-        public Cliente(String nome,int idade,String cidade,String cpf) {
-            this.nome = nome;
-            this.idade = idade;
-            this.cidade = cidade;
-            this.cpf = cpf;
-        }
+    private String nome;
+    private int idade;
+    private String cidade;
+    private String cpf;
 
-        public void exibirInformacoes() {
+    public Cliente(
+            String nome,
+            int idade,
+            String cidade,
+            String cpf
+    ) {
 
-            System.out.println(
-                    "Nome: " + getNome()
-            );
-
-            System.out.println(
-                    "Idade: " + getIdade() + " anos"
-            );
-
-            System.out.println(
-                    "Cidade: " + getCidade()
-            );
-
-            System.out.println(
-                    "CPF: " + getCpf()
-            );
-        }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public void setIdade(int idade) {
-            this.idade = idade;
-        }
-
-        public void setCidade(String cidade) {
-            this.cidade = cidade;
-        }
-
-        public void setCpf(String cpf) {
-            this.cpf = cpf;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public int getIdade() {
-            return idade;
-        }
-
-        public String getCidade() {
-            return cidade;
-        }
-
-        public String getCpf() {
-            return cpf;
-        }
+        this.nome = nome;
+        this.idade = idade;
+        this.cidade = cidade;
+        this.cpf = cpf;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome do Cliente: " + this.getNome() + " | " + "idade: " + this.getIdade() + " | " + "CPF: " + this.getCpf() + " | " + "Cidade: " + this.getCidade();
+    }
+}
